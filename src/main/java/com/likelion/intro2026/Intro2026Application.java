6cdfe83d86eec25024f0d84e668a751d557a51fa
baseballclub <abkimdoyo25@gmail.com>
2025-12-19 13:36:26 +0900

    init: spring boot project setup

---
+++ src/main/java/com/likelion/intro2026/Intro2026Application.java
@@ -0,0 +1,13 @@
+package com.likelion.intro2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Intro2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Intro2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/intro2026/Intro2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.intro2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Intro2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
